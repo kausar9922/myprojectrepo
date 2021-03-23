8d685efb1ba1289406f5b3a8ef5af828fa7ffb92
Kausar <Kausar@KausarShaikh>
2021-03-23 10:32:33 +0530

    added new fifth func in master

--- SampleGitDemo/src/Calculator.java
+++ SampleGitDemo/src/Calculator.java
@@ -31,4 +31,7 @@ public class Calculator {
 	public static void quad(int num1){
 		 System.out.println(num1*num1*num1*num1);
 	}
+	public static void fifth(int num1){
+		 System.out.println(num1*num1*num1*num1*num1);
+	}
 }
