8691a33e717f544809f114c776fa4ea561945be4
Kausar <Kausar@KausarShaikh>
2021-03-23 10:54:26 +0530

    added br1 func

--- SampleGitDemo/src/Calculator.java
+++ SampleGitDemo/src/Calculator.java
@@ -34,4 +34,7 @@ public class Calculator {
 	public static void fifth(int num1){
 		 System.out.println(num1*num1*num1*num1*num1);
 	}
+	public static void br1(int num1){
+		 System.out.println(num1*num1);
+	}
 }
